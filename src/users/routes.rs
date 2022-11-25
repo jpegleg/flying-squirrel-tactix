c8f7bccd7057123b02b9fb9da030e2800def1dd7
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2022-11-25 13:50:25 -0700

    Update routes.rs

--- src/users/routes.rs
+++ src/users/routes.rs
@@ -12,11 +12,11 @@ async fn healthchecks(req: HttpRequest) -> Result<HttpResponse, CustomError> {
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel-tactix - /health GET (health check) request - from {:?} - {:?}", readi, peer, &requ);
+    println!("[{:?} INFO ] - flying-squirrel-tactix - /health GET (health check) request - from {:?} - {:?}", readi, peer, &requ);
     let users = Users::find(1)?;
     let readb: DateTime<Utc> = Utc::now();
     match users {
-      users => println!("[{:?} INFO ] - squirrel-tactix - /health GET (health check) response OK - {:?}", readb, requ),
+      users => println!("[{:?} INFO ] - flying-squirrel-tactix - /health GET (health check) response OK - {:?}", readb, requ),
     };
     let hresp = "RECV";
     Ok(HttpResponse::Ok().json(hresp))
@@ -27,7 +27,7 @@ async fn find_all(req: HttpRequest) -> Result<HttpResponse, CustomError> {
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel-tactix - /users GET function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - flying-squirrel-tactix - /users GET function request - from {:?} - {:?}", readi, peer, requ);
     let users = Users::find_all()?;
     Ok(HttpResponse::Ok().json(users))
 }
@@ -37,7 +37,7 @@ async fn find(req: HttpRequest, id: web::Path<i32>) -> Result<HttpResponse, Cust
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel-tactix - /users/{id} GET function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - flying-squirrel-tactix - /users/{id} GET function request - from {:?} - {:?}", readi, peer, requ);
 
 
     let user = Users::find(id.into_inner())?;
@@ -49,7 +49,7 @@ async fn create(req: HttpRequest, user: web::Json<User>) -> Result<HttpResponse,
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel-tactix - /users POST function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - flying-squirrel-tactix - /users POST function request - from {:?} - {:?}", readi, peer, requ);
 
     let user = Users::create(user.into_inner())?;
     Ok(HttpResponse::Ok().json(user))
@@ -65,7 +65,7 @@ async fn update(
     let requ = req.headers();
     let user = Users::update(id.into_inner(), user.into_inner())?;
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel-tactix - /users/x (update) PUT function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - flying-squirrel-tactix - /users/x (update) PUT function request - from {:?} - {:?}", readi, peer, requ);
 
 
     Ok(HttpResponse::Ok().json(user))
@@ -76,7 +76,7 @@ async fn delete(req: HttpRequest, id: web::Path<i32>) -> Result<HttpResponse, Cu
     let peer = req.peer_addr();
     let requ = req.headers();
     let readi: DateTime<Utc> = Utc::now();
-    println!("[{:?} INFO ] - squirrel-tactix - /users/{id} DELETE function request - from {:?} - {:?}", readi, peer, requ);
+    println!("[{:?} INFO ] - flying-squirrel-tactix - /users/{id} DELETE function request - from {:?} - {:?}", readi, peer, requ);
 
     let deleted_user = Users::delete(id.into_inner())?;
     Ok(HttpResponse::Ok().json(json!({ "deleted": deleted_user })))
